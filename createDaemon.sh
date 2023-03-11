aa7a35c6497338aa6a52cdc59a28ff586ba24e86
意味がない <66781460+Nenind@users.noreply.github.com>
2023-03-12 00:38:27 +0500

    script for creating daemon hotfix

--- createDaemon.sh
+++ createDaemon.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-touch /etc/systemd/system/Makima-Bot.service
+name=$(basename "$PWD")
+touch /etc/systemd/system/$name.service
 
-cat > /etc/systemd/system/Makima-Bot.service <<EOF 
+cat > /etc/systemd/system/$name.service <<EOF 
 [Unit]
 Description="Best bot 4ever!"
 
+
 [Service]
 User=ubuntu
-WorkingDirectory=/home/ubuntu/Makima-Bot
-VIRTUAL_ENV=/home/ubuntu/Makima-Bot/venv
+WorkingDirectory=$(pwd)
+VIRTUAL_ENV=$(pwd)/venv
 Environment=PATH=$VIRTUAL_ENV/bin:$PATH
-ExecStart=/home/ubuntu/Makima-Bot/venv/bin/python main.py
+ExecStart=$(pwd)/venv/bin/python main.py
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-
-python3 -m venv venv
 python3 -m venv venv
 ./venv/bin/python -m pip install --upgrade pip
 ./venv/bin/pip install -r requirements.txt
 
 systemctl daemon-reload
-systemctl start Makima-Bot.service
-systemctl enable Makima-Bot.service
-systemctl status Makima-Bot.service
+systemctl start $name.service
+systemctl enable $name.service
+systemctl status $name.service

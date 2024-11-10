02c707326b8f7cf732965d42f9e70d7c80fd4a5b
Terrashes <Terrashes>
2024-11-10 13:52:33 +0300

    add youtube playing

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import random
 from datetime import *
 from urllib import response
 from urllib.error import URLError
+import yt_dlp
 
 import discord
 import requests
@@ -43,7 +44,10 @@ def get_prefix(client, message):
     return config["servers"][str(message.guild.id)]["prefix"], 'm!'
 
 
-intents = discord.Intents.all()
+intents = discord.Intents.default()
+intents.message_content = True
+intents.voice_states = True
+
 startupDate = datetime.now(timezone.utc)
 bot = commands.Bot(command_prefix = get_prefix, help_command=None, intents=intents)
 
@@ -303,6 +307,50 @@ async def on_member_remove(Member):
     channel = bot.get_channel(int(config["servers"][str(Member.guild.id)]["leaveMessageChannel"]))
     await channel.send(config["servers"][str(Member.guild.id)]["leaveMessage"].format(str(Member.name)+"#"+str(Member.discriminator)))
 
+
+FFMPEG_OPTIONS = {'options': '-vn'}
+YDL_OPTIONS = {
+    'format': 'bestaudio',
+    'noplaylist': True,
+    'cookiefile': 'yt_cookies.txt'
+    }
+yt_queue = []
+
+
+@bot.command()
+async def play(ctx, *, search):
+    voice_channel = ctx.author.voice.channel if ctx.author.voice else None
+    if not voice_channel:
+        return await ctx.send("You're not in voice channel")
+    if not ctx.voice_client:
+        await voice_channel.connect()
+
+    async with ctx.typing():
+        with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(f"ytsearch:{search}", download=False)
+            if 'entries' in info:
+                info = info['entries'][0]
+            url = info['url']
+            title = info['title']
+            yt_queue.append((url, title))
+            await ctx.send(f"Added to queue: **{title}**")
+    if not ctx.voice_client.is_playing():
+        await play_next(ctx=ctx)
+
+
+async def play_next(ctx):
+    if yt_queue:
+        url, title = yt_queue.pop(0)
+        source = await discord.FFmpegOpusAudio.from_probe(url, **FFMPEG_OPTIONS)
+        ctx.voice_client.play(source, after=lambda _: bot.loop.create_task(play_next(ctx)))
+    elif not ctx.voice_client.is_playing():
+        await ctx.send('Queue is empty')
+
+@bot.command()
+async def skip(ctx):
+    if ctx.voice_client and ctx.voice_client.is_playing():
+        ctx.voice_client.stop()
+        await ctx.send('Skipped')
 
 async def load_extensions():
     # for filename in os.listdir("./"):
